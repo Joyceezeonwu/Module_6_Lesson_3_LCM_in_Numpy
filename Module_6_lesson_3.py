12f210cb8377fba06bd89069a072b3be755e4c17
Joyce Ezeonwu <108493302+Joyceezeonwu@users.noreply.github.com>
2022-09-03 00:08:03 -0700

    Update Module_6_lesson_3.py

--- Module_6_lesson_3.py
+++ Module_6_lesson_3.py
@@ -4,16 +4,16 @@ import numpy
 import numpy as npy
 # print("successful")
 
+#Generating an array of numbers between 1 and 100 with both numbers included 
 list = npy.arange(1, 101)
-# print(list)
+print(list)
 
-
+#Generating all the even integers from 1 to 100 in the list
 array1 = npy.arange(2,101,2)
-# print("Array of all the even integers from 1 to 100")
-# print(array1)
+print(array1)
 
-arr = npy.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100])
-x = npy.lcm.reduce(arr)
-print(x)
+x = npy.lcm.reduce(array1)
+print('\nThe LCM of the Even numbers in the array is: ', x)
+
 
 
